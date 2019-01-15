cdde31e129ea26a7e40b9c97b3fb29585df37da6
zhangype <1141404898@qq.com>
2019-01-15 17:38:48 +0800

    初始化项目

---
+++ src/main/java/com/zhangype/myhadoop/MaxTemperature.java
@@ -0,0 +1,53 @@
+package com.zhangype.myhadoop;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+/**
+ * @author danielzhang@webank.com
+ * @version V1.0.0
+ * @date 2018/10/12
+ */
+public class MaxTemperature {
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: MaxTemperature <input path> <output path>");
+            System.exit(-1);
+        }
+
+        Configuration conf = new Configuration();
+//        System.setProperty("hadoop.home.dir", "E:/hadoop/hadoop-2.3.0");
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        if (otherArgs.length != 2) {
+            System.err.println("Usage: wordcount <in> <out>");
+            System.exit(2);
+        }
+
+        Job job = new Job(conf, "MaxJob");
+        job.setJobName("Max temperature");
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        // 通过setMapperClass()和setReducerClass指定要用的mapper类型和reducer类型
+        job.setMapperClass(MaxTemperatureMapper.class);
+        job.setReducerClass(MaxTemperatureReducer.class);
+
+        /*
+        setOutputKeyClass()方法和setOutputValueClass()方法控制reduce函数的输出类型，并且必须和 Reduce 类产生的相匹配。
+        map函数的输出类型默认情况下和reduce函数是相同的，因此如果mapper产生出和reducer不相同的类型时，
+        则必须通过setMapOutputKeyClass()和setMapOutputValueClass()方法来设置map函数的输出类型
+         */
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
---
+++ src/main/java/com/zhangype/myhadoop/MaxTemperatureReducer.java
@@ -0,0 +1,24 @@
+package com.zhangype.myhadoop;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+/**
+ * @author danielzhang@webank.com
+ * @version V1.0.0
+ * @date 2018/10/12
+ */
+public class MaxTemperatureReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        int maxValue = Integer.MIN_VALUE;
+        for (IntWritable value : values) {
+            maxValue = Math.max(maxValue, value.get());
+        }
+        context.write(key, new IntWritable(maxValue));
+    }
+}
